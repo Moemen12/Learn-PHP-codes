7d1fd1bfabd59ca855554140985008b82a2a1fd6
moemen saadeh <99843903+Moemen12@users.noreply.github.com>
2022-10-17 00:46:43 +0300

    Update oop.php

--- oop.php
+++ oop.php
@@ -76,7 +76,70 @@
   
   
   
+  /------------------------------------------------------------------
   
+  
+  <?php
+  
+  
+  class AppleDevice{
+  
+  public $ram = '1GB'; // All properties here will be default properties
+  public $space = '6GB'; 
+  public $inch = '3.3 inch';
+  public $color = 'blue';
+  private $lock;
+  
+ // Method
+ public function ChangeSpec($ra,$in,$sp,$co){
+     $this->ram= $ra;
+     $this->inch=$in;
+     $this->space=$sp;
+     $this->color=$co;
+ }
+  
+  public function changeLock($lo){
+      $this->lock = sha1($lo) ;
+  }
+  }
+
+  
+  $iphone6plus = new AppleDevice();
+  $iphone6plus->ChangeSpec('4GB','7inch','32GB','gold');
+  $iphone6plus->changeLock('moemren123');
+   
+   echo '<pre>';
+   var_dump($iphone6plus);
+   echo '</pre>';
+  
+ // echo $iphone6plus->lock;  
+ // Cannot access private property AppleDevice::$lock
+  
+   $iphone7plus = new AppleDevice();
+$iphone7plus->ChangeSpec('16GB','37inch','64GB','white');
+
+
+  echo '<pre>';
+  var_dump($iphone7plus);
+  echo '</pre>';
+  
+  $iphone = new AppleDevice();
+  echo '<pre>';
+  var_dump($iphone);
+  echo '</pre>';
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+?>
   
   
   
