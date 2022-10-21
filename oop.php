955a1745d18cae91b828b5ba5fc4ba1b4324d1ef
moemen saadeh <99843903+Moemen12@users.noreply.github.com>
2022-10-21 18:06:46 +0300

    Update oop.php

--- oop.php
+++ oop.php
@@ -384,44 +384,208 @@ $phone->WelcomeToApp("Moemen","2GB");
 
 ?>
  /------------------------------------------------------------------
+ <?php
+  class Iphone{
+      public static $name = "osama";
+      public static $ram = "4GB";
+      
+      public static function sayHello(){
+          return "Hello";
+      }
+  }
+  
+  
+  echo Iphone::$name;
+  echo Iphone::$ram;
+  echo Iphone::sayHello();
+?>
+
+
+
+<?php
+  class Iphone{
+      public $name;
+      public $ram ;
+      
+      public function pressPower(){
+          echo "You have pressed the power button";
+          return $this;
+      }
+       public function bootPhone(){
+          echo "The Phone is booting now";
+          return $this;
+      }
+       public function sayWelcome(){
+          echo "Welcome to Phone";
+          return $this;
+      }
+  }
+  
+  
+ $phone = new Iphone();
+  $phone->pressPower()->bootPhone()->sayWelcome();
+ echo "<pre>";
+ print_r($phone);
+ echo "</pre>";
+ 
+?>
+ /------------------------------------------------------------------
+ <?php
+
+ trait fingerPrint{
+     
+     public function fingerFeature(){
+         echo "This is finger print feature\n";
+         return $this;
+     }
+ }
+
+ trait ThreeDimensionsTouch{
+     
+     public function threeD(){
+         echo "This is 3D touch feature\n";
+         return $this;
+     }
+ }
+
+ trait FaceDectect{
+     
+     public function faceFeature(){
+         echo "This is face detect feature\n";
+         return $this;
+     }
+ }
+ 
+ 
+ trait AllFeatures{
+    use fingerPrint,ThreeDimensionsTouch,FaceDectect;
+ }
+
+ 
+ 
+ 
+ 
+
+  class Iphone{
+     use AllFeatures;
+     function sayHello(){
+         echo "Hello from IPhone";
+         return $this;
+     }
+  }
+  
+  
+  
+    class Sony{
+  use FaceDectect;
+     
+     function sayHello(){
+         echo "Hello from Sony";
+     }
+  }
+  
+   class Nokia{
+    function sayHello(){
+         echo "Hello from Nokia";
+     }
+  }
+  
+  
+  
+ $phone = new Iphone();
+ $phone->fingerFeature()->threeD()->faceFeature()->sayHello();
+ echo "<pre>";
+ print_r($phone);
+ echo "</pre>";
+ 
+ 
+ 
+ $sony = new Sony();
+ echo "<pre>";
+ print_r($sony);
+ echo "</pre>";
+ $sony->faceFeature()->sayHello();
+ 
+ 
+ $nokia = new Nokia();
+ echo "<pre>";
+ print_r($nokia);
+ echo "</pre>";
+ $nokia->sayHello();
+?>
+
+ /------------------------------------------------------------------
+ 
+ <?php
+trait MyFeature{
+      public function sayHello(){
+        echo "Hello from trait";
+    }
+}
+
+
+class AppleDevice{
+    public function sayHello(){
+        echo "Hello from class";
+    }
+}
+  
+  class Iphone extends AppleDevice{
+      use MyFeature;
+  }
+  
+ $phone = new Iphone();
+ $phone->sayHello();
+ echo "<pre>";
+ print_r($phone);
+ echo "</pre>";
+ 
  
  
 ?>
 
+ /------------------------------------------------------------------
+ 
+ <?php
+trait MyFeature1{
+      public function feature(){
+        echo "This is feature no 1";
+    }
+}
+
+trait MyFeature2{
+      public function feature(){
+        echo "This is feature no 2";
+    }
+}
+
+  
+  class Iphone {
+      use MyFeature1,MyFeature2{
+          MyFeature2::feature as MoemenFeat;
+          MyFeature1::feature insteadof MyFeature2;
+      }
+  }
+  
+ $phone = new Iphone();
+ $phone->feature();
+ $phone->MoemenFeat();
+ echo "<pre>";
+ print_r($phone);
+ echo "</pre>";
+ 
+ 
+ 
+ /------------------------------------------------------------------
+?>
 
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
 ?>
   
-  
-  
-  
-  
-  
-  
+ 
   
 ?>
   
-  
-  
-  
  
-  
-  
   
 ?>
